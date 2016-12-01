65c17b7bb14334440596649a1bfb7aacbc117512
Csaba Todor <todorcs@gmail.com>
2016-12-01 10:07:41 -0600

    Updated Header Component
    Added Boostrap navigation into the View
    Added User Avatar and Username into the View
    Removed inline CSS Styles
    Set user profile information to local Storage

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,38 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+// Services
 import { Auth } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styles: [
-    `header {
-      background: #eee;
-      width: 100%;
-      padding: 20px 0;
-    }
-    .logo {
-      margin: 0 20px;
-      font-size: 18px;
-      font-weight: bold;
-      text-decoration: none;
-    }
-    .login-btn,
-    .logout-btn {
-      float: right;
-      margin: 0 20px;
-    }
-    .user-menu {
-      float: right;
-      margin: 0;
-    }
-    .user-menu > li {
-      display: inline-block;
-    }`
-  ]
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private auth: Auth) {}
+  profile: any;
+
+  constructor(private auth: Auth) {
+    this.profile = JSON.parse(localStorage.getItem('profile'));
+  }
 
   ngOnInit() {
   }
