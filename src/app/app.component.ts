9df055d0f5302e799b5bb4e4757d37bef373da4c
Csaba Todor <todorcs@gmail.com>
2016-12-01 08:49:24 -0600

    Moved Logo & Buttons from App Component to Header Component
    added Inline CSS styles for Header View

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Auth } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +6,5 @@ import { Auth } from './auth/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private auth: Auth) {}
+
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,13 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Auth } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: []
+  styles: [
+    `header {
+      background: #eee;
+      width: 100%;
+      padding: 20px 0;
+    }
+    .logo {
+      margin: 0 20px;
+      font-size: 18px;
+      font-weight: bold;
+      text-decoration: none;
+    }
+    .login-btn,
+    .logout-btn {
+      float: right;
+      margin: 0 20px;
+    }
+    .user-menu {
+      float: right;
+      margin: 0;
+    }
+    .user-menu > li {
+      display: inline-block;
+    }`
+  ]
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: Auth) {}
 
   ngOnInit() {
   }
