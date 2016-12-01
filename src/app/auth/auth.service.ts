8acff162be84613262afee122113a6aa2d2e84a2
Csaba Todor <todorcs@gmail.com>
2016-12-01 10:09:58 -0600

    Updated Auth Service to get profile information on Authentication

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,18 +1,28 @@
-import { Injectable }      from '@angular/core';
+import { Injectable } from '@angular/core';
 import { tokenNotExpired } from 'angular2-jwt';
 
 // Avoid name not found warnings
-declare var Auth0Lock: any;
+// declare var Auth0Lock: any;
+let Auth0Lock = require('auth0-lock').default;
 
 @Injectable()
 export class Auth {
   // Configure Auth0
   lock = new Auth0Lock('bY159CtFcxmfocFXDe4HMzTA3rKTncIF', 'cstodor.eu.auth0.com', {});
+  profile: any;
 
   constructor() {
+    // Auth Lock Pop Up Registration
     // Add callback for lock `authenticated` event
-    this.lock.on("authenticated", (authResult) => {
-      localStorage.setItem('id_token', authResult.idToken);
+    this.lock.on("authenticated", (authResult: any) => {
+      this.lock.getProfile(authResult.idToken, function (error: any, profile: any) {
+        if (error) {
+          throw new Error(error);
+        }
+        localStorage.setItem('id_token', authResult.idToken);
+        localStorage.setItem('profile', JSON.stringify(profile)); // We will wrap the profile into a JSON object as In local Storage you can only store strings
+        console.log('PROFILE: ' + profile);
+      });
     });
   }
 
@@ -30,5 +40,6 @@ export class Auth {
   public logout() {
     // Remove token from localStorage
     localStorage.removeItem('id_token');
+    localStorage.removeItem('profile');
   };
 }
